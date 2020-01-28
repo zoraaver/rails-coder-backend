256907db5ae3e610a4acfe654abf238cac6c140e
github username <github email address>
2020-01-28 16:37:07 +0000

    hey

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+
+  def token
+    JWT.encode({user_id: self.id}, ENV["CODER"])
+  end
+
+end
