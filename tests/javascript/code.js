23947491546027933a78bac6bfa8a45f202c2241
github username <github email address>
2020-02-04 13:25:08 +0000

    added code.js to gitignore

--- tests/javascript/code.js
+++ tests/javascript/code.js
@@ -1,8 +1,3 @@
-function factorial(n) {
-    return n == 1? 1 : n*factorial(n - 1)
-}
-sdddf
-
-function helloWorld() {
-    return "HEY there!"
+function ascendingSum(arr) {
+    return [1,3,6,10];
 }
--- tests/javascript/test.js
+++ tests/javascript/test.js
@@ -1,28 +1,21 @@
 const fs = require('fs');
-var assert = require('chai').assert;
+const assert = require('chai').assert;
+const expect = require('chai').expect;
 const sourceFile = fs.readFileSync('tests/javascript/code.js', 'utf-8');
 const jsdom = require('mocha-jsdom')
 jsdom({ src: sourceFile, url: "http://localhost" })
 
-
-
-describe('#helloWorld', function () {
-    it('should return "HEY there!"', function () {
-        assert.equal(helloWorld(), "HEY there!");
+describe('#ascendingSum', function () {
+    it('should return an array of the sorted partial sums for positive numbers', function () {
+        expect(ascendingSum([1,2,3,4])).to.deep.equal([1,3,6,10]);
     })
+
+   it('should also work for negative numbers', function () {
+        expect(ascendingSum([0,-1,110,2])).to.deep.equal([-1,-1,1,111]);
+   }) 
+
+   it('should allow for non-integer values', function () {
+       expect(ascendingSum([1.1,5.45,2.3,-2.3])).to.deep.equal([-2.3, -1.2, 1,1, 6.55])
+   })
+
 })
-
-describe('Array', function() {
-    describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function(){
-            assert.equal(-1, [1,2,3].indexOf(4));
-        });
-    });
-});
-
-describe('#factorial', function () {
-    it('should return 720 given input 6', function () {
-        assert.equal(720, factorial(6));
-    })
-})
-
