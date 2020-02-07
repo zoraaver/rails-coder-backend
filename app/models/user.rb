cb49777f3b6f1bc0af8c54fa1d1f0013ac54c3f6
github username <github email address>
2020-02-07 15:18:28 +0000

    added destroy dependencies for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
 
-  has_many :user_courses
+  has_many :user_courses, dependent: :destroy
   has_many :courses, through: :user_courses
 
-  has_many :user_lessons
+  has_many :user_lessons, dependent: :destroy
   has_many :lessons, through: :user_lessons
 
   has_secure_password
