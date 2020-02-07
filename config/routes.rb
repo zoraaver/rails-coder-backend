6a76b39ba5513430d15b23b0a79c0e243f7c0440
github username <github email address>
2020-02-07 15:21:47 +0000

    added admin column to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :update, :destroy]
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show, :update]
   resources :lessons, only: [:show, :update]
   get "/validate", to: "users#validate"
   post "/login", to: "users#login"
