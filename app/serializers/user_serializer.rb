5539258a607071972c0a1e27fa81e10883d529f8
zoraaver <zoraaver@gmail.com>
2020-02-11 09:53:22 +0000

    added admin to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :email, :token, :courses
+  attributes :email, :token, :courses, :admin
 
   def courses
     object.courses.map{|c| {id: c.id, title: c.title, description: c.description, img_url: c.img_url}}
