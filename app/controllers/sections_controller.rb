6db402f860699123f6bbc15ad004b78b3ea64d73
github username <github email address>
2020-02-12 13:16:29 +0000

    finished section routes

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,11 +5,18 @@ class SectionsController < ApplicationController
   def create
     last_sort_id = Course.find(section_params[:course_id]).sections.order(:sort_id).pluck(:sort_id).last
     section = Section.create(title: section_params[:title], sort_id: last_sort_id + 1 || 0, course_id: section_params[:course_id])
-    render json: section, scope: {user: @current_user}
+
+    if section.valid?
+      render json: section, scope: {user: @current_user}
+    else
+      render json: {message: section.errors.full_messages[0]}, status: :not_acceptable
+    end
   end
 
   def update
-    byebug
+    section = Section.find(params[:id])
+    section.update(section_params) 
+    render json: {id: section.id, title: section.title}
   end
 
   def destroy
@@ -21,6 +28,6 @@ class SectionsController < ApplicationController
   private
 
   def section_params
-    params.require(:section).permit(:title, :course_id, :sort_id)
+    params.require(:section).permit(:title, :course_id, :id, :sort_id)
   end
 end
